2473ad418c97419b692e5d5e810f79ebed24ba78
Susan VanderPlas <srvanderplas@gmail.com>
2014-12-07 20:19:00 -0600

    Add help text, change title, and add (s) to groups

--- ui.R
+++ ui.R
@@ -10,25 +10,28 @@ shinyUI(fluidPage(
 #   tags$head(tags$style(type="text/css", "label.radio { display: inline; width: 25%; margin-right:2%;}")), # format checkbox inputs in 3 columns with some padding.
   
   # Application title
-  titlePanel("Soybean Yield"),
+  titlePanel("Soybean Phenology and Relative Yield"),
   
   wellPanel(
     fluidRow(
-      column(2, offset=1, radioButtons("compare", label="Comparison Variable", 
-                                       choices=c("Location"="Location", "Planting Date"="PlantDay", "Maturity Group"="MG"))),
+      column(3, helpText("Select a variable to compare up to 3 different values for that variable. You may only select one variable at a time to compare in this way.")),
+      column(9, helpText("Click on the boxes below to see what options are available. You can type or select options to add them, and use the backspace key to remove a selected option. You may select up to three values for the variable chosen as the comparison variable, and one value for the other two variables."))
+      ),
+    fluidRow(
+      column(2, offset=1, 
+             radioButtons("compare", label="Comparison Variable", 
+                          choices=c("Location"="Location", "Planting Date"="PlantDay", "Maturity Group"="MG"))),
       column(2,div(align="center", 
                    selectizeInput("location", label="Select location(s)", 
                                   choices=locations, selected="Ames", 
                                   multiple=TRUE, options=list(maxItems=3)))),
       column(2,div(align="center", 
-                   selectizeInput("planting", label="Select planting date", 
-                                  choices=planting.date, selected="5-Jun", 
+                   selectizeInput("planting", label="Select planting date(s)", 
+                                  choices=planting.date, selected="15-May", 
                                   multiple=TRUE, options=list(maxItems=3)))),
       column(2,div(align="center", 
-                   selectizeInput("maturity", label="Select maturity group", 
-                                  choices=0:5, selected=0, multiple=TRUE, options=list(maxItems=3)))),
-      column(2,div(align="center", 
-                   checkboxInput("failed", label="Include failed trials?")))
+                   selectizeInput("maturity", label="Select maturity group(s)", 
+                                  choices=0:5, selected=2, multiple=TRUE, options=list(maxItems=3))))
     )),
   fluidRow(
     plotOutput("DevelopmentPlot", height=350),
