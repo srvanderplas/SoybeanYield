c83d65ae0aec98a4b4a24b100122d5e57275c622
Susan VanderPlas <srvanderplas@gmail.com>
2014-12-07 21:07:48 -0600

    Fix frost date - jitter hour a bit so that areas with the same frost date still have different lines.

--- server.R
+++ server.R
@@ -65,7 +65,6 @@ shinyServer(function(input, output, session) {
         xlab("") + ylab("") + theme_bw() + 
         theme(axis.text=element_blank(), axis.ticks=element_blank(), title=element_blank())
     } else {
-      
       textdata <- longdata.sub%>%group_by(Location, PlantDay, MG, Stage) %>% do(fix.na.data(.))
       textdata <- merge(textdata, longdata.sub%>%group_by(Stage)%>%summarize(y=mean(Date, na.rm=T), ymax=max(Date, na.rm=T)))
       if(sum(is.na(textdata$text))>0){
@@ -95,6 +94,7 @@ shinyServer(function(input, output, session) {
         summarize(frost.date=floor_date(quantile(Date.of.first.frost2, .5, na.rm=T), "day"), 
                   y=.5, 
                   label="First Frost Likely") %>% as.data.frame()
+      hour(frost.date.df$frost.date) <- sample(0:11, nrow(frost.date.df))
       
       frost.date.df$frost.date.lb <- floor_date(quantile(filter(yield, MG%in%input$maturity & 
                                                                   Location%in%input$location & 
@@ -114,9 +114,9 @@ shinyServer(function(input, output, session) {
         coord_flip() + 
         scale_color_brewer(gsub("PlantDay", "Planting\nDate", input$compare), palette="Set1") + 
         scale_fill_brewer(gsub("PlantDay", "Planting\nDate", input$compare), palette="Set1") + 
-        geom_rect(aes(ymin=frost.date.lb, ymax=frost.date.ub, xmin=-Inf, xmax=Inf), alpha=.1, fill="black", data=frost.date.df) +  
+        geom_rect(aes(ymin=frost.date.lb, ymax=frost.date.ub, xmin=-Inf, xmax=Inf), alpha=.05, fill="black", data=frost.date.df) +  
         geom_segment(aes(y=frost.date, yend=frost.date, x=y+.25, xend=Inf, color=facet), data=frost.date.df, linetype=2) + 
-        geom_text(aes(y=textlabel, x=y, label=label), data=frost.date.df, hjust=1, vjust=0) + 
+        geom_text(aes(y=textlabel, x=y, label=label), data=frost.date.df, hjust=1, vjust=0, size=6) + 
         xlab("") + ylab("") + 
         geom_vline(aes(xintercept=seq(.5, 5.5, 1)), colour="grey30") +
         theme_bw() + 
