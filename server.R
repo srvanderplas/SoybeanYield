0d9a05ee537cd80d9226d26399fca61a37bb6ed6
Susan VanderPlas <srvanderplas@gmail.com>
2014-12-07 21:48:50 -0600

    fix error with scales

--- server.R
+++ server.R
@@ -82,7 +82,7 @@ shinyServer(function(input, output, session) {
       }
       
       quantile.sub <- longdata.sub %>% 
-        group_by(Location, PlantDay, MG, Stage, facet) %>% 
+        group_by(Location, PlantDay, MG, facet, Stage) %>% 
         summarize(q25=floor_date(quantile(Date, .25, na.rm=T), "day"),
                   q50=floor_date(quantile(Date, .5, na.rm=T), "day"), 
                   q75=floor_date(quantile(Date, .75, na.rm=T), "day"))
@@ -104,7 +104,7 @@ shinyServer(function(input, output, session) {
                                                                   Location%in%input$location & 
                                                                   PlantDay%in%input$planting)$Date.of.first.frost2, 
                                                          .75, na.rm=T), "day")
-      frost.date.df$textlabel <- median(frost.date.df$frost.date)
+      frost.date.df$textlabel <- floor_date(median(frost.date.df$frost.date), "day")
       frost.date.df$facet <- frost.date.df[,input$compare]
       
       plot <- ggplot() + 
@@ -115,7 +115,7 @@ shinyServer(function(input, output, session) {
         scale_color_brewer(gsub("PlantDay", "Planting\nDate", input$compare), palette="Set1") + 
         scale_fill_brewer(gsub("PlantDay", "Planting\nDate", input$compare), palette="Set1") + 
         geom_rect(aes(ymin=frost.date.lb, ymax=frost.date.ub, xmin=-Inf, xmax=Inf), alpha=.05, fill="black", data=frost.date.df) +  
-        geom_segment(aes(y=frost.date, yend=frost.date, x=y+.25, xend=Inf, color=facet), data=frost.date.df, linetype=2) + 
+        geom_segment(aes(y=frost.date, yend=frost.date, x=y+.25, xend=Inf, color=factor(facet)), data=frost.date.df, linetype=2) + 
         geom_text(aes(y=textlabel, x=y, label=label), data=frost.date.df, hjust=1, vjust=0, size=6) + 
         xlab("") + ylab("") + 
         geom_vline(aes(xintercept=seq(.5, 5.5, 1)), colour="grey30") +
@@ -144,6 +144,7 @@ shinyServer(function(input, output, session) {
     plotdata <- filter(yield, 
                        Location%in%input$location &
                        PlantDay%in%input$planting)
+    plotdata$Location <- factor(plotdata$Location, levels=input$location, ordered=T)
     if(input$compare!="MG"){
       plotdata$facet <- plotdata[,input$compare]
     } else {
@@ -160,10 +161,18 @@ shinyServer(function(input, output, session) {
     spline.data <- plotdata %>% group_by(facet) %>% do({
       bx3 <- cbind(I=1, ns(.$jitterMG, df=3)) 
       cubicspline3 <- lm(data=., nyield~bx3-1)
-      tmp <- data.frame(jitterMG=.$jitterMG)
+      tmp <- data.frame(MG=.$MG, jitterMG=.$jitterMG)
       tmp <- cbind(tmp, suppressWarnings(predict(cubicspline3, se.fit=T, interval=input$intervaltype, level=as.numeric(input$pvalue))))
       tmp
     })
+
+    spline.max <- spline.data %>% group_by(facet) %>% do({
+      .[which.max(.$fit.fit),]
+    })
+  
+    if(nrow(spline.max)>1 & length(unique(spline.max$MG))<nrow(spline.max)){
+      spline.max$MG <- spline.max$MG + seq(-.05, .05, length.out = nrow(spline.max))
+    }
     
     if(input$points){
       plot <- ggplot() + 
@@ -171,7 +180,7 @@ shinyServer(function(input, output, session) {
     } else {
       plot <- ggplot()
     }
-    
+
     if(sum(is.na(plotdata$facet))>0){
       plot <- plot + 
         geom_line(data=spline.data, aes(x=jitterMG, y=fit.lwr), linetype=2) + 
@@ -182,10 +191,11 @@ shinyServer(function(input, output, session) {
         geom_line(data=spline.data, aes(x=jitterMG, y=fit.lwr, colour=factor(facet)), linetype=2) + 
         geom_line(data=spline.data, aes(x=jitterMG, y=fit.upr, colour=factor(facet)), linetype=2)  + 
         geom_line(data=spline.data, aes(x=jitterMG, y=fit.fit, colour=factor(facet))) +
-        scale_colour_brewer(gsub("PlantDay", "Planting\nDate", input$compare),palette="Set1")
+        scale_colour_brewer(gsub("PlantDay", "Planting\nDate", input$compare),palette="Set1") + 
+        geom_segment(data=spline.max, aes(x=MG, y=fit.fit, xend=MG, yend=0, colour=factor(facet)), linetype=3)
     }
     plot <- plot + 
-        scale_y_continuous(breaks=c(0, .25, .5, .75, 1), name="Relative Yield", limits=c(0, 1.05)) + 
+        scale_y_continuous(breaks=c(0, .25, .5, .75, 1), name="Relative Yield", limits=c(0, 1.1)) + 
         scale_x_continuous(breaks=0:5, labels=0:5, name="Maturity Group") + 
         theme_bw() + 
         theme(plot.title = element_text(size = 18), 
@@ -202,6 +212,7 @@ shinyServer(function(input, output, session) {
     plotdata <- filter(yield, 
                        Location%in%input$location &
                          MG%in%input$maturity)
+    plotdata$Location <- factor(plotdata$Location, levels=input$location, ordered=T)
     if(input$compare!="PlantDay"){
       plotdata$facet <- plotdata[,input$compare]
     } else {
@@ -222,7 +233,11 @@ shinyServer(function(input, output, session) {
       tmp <- cbind(tmp, suppressWarnings(predict(cubicspline5, se.fit=T, interval=input$intervaltype, level=as.numeric(input$pvalue))))
       tmp
     })
-    
+
+    spline.max <- spline.data %>% group_by(facet) %>% do({
+      .[which.max(.$fit.fit),]
+    })
+
     if(input$points){
       plot <- ggplot() + 
         geom_jitter(data=plotdata, aes(x=jitterDate, y=nyield), alpha=.25) 
@@ -240,10 +255,11 @@ shinyServer(function(input, output, session) {
         geom_line(data=spline.data, aes(x=jitterDate, y=fit.lwr, colour=factor(facet)), linetype=2) + 
         geom_line(data=spline.data, aes(x=jitterDate, y=fit.upr, colour=factor(facet)), linetype=2) + 
         geom_line(data=spline.data, aes(x=jitterDate, y=fit.fit, colour=factor(facet))) + 
-        scale_colour_brewer(gsub("PlantDay", "Planting\nDate", input$compare),palette="Set1") 
+        scale_colour_brewer(gsub("PlantDay", "Planting\nDate", input$compare),palette="Set1") + 
+        geom_segment(data=spline.max, aes(x=jitterDate, y=fit.fit, xend=jitterDate, yend=0, colour=factor(facet)), linetype=3)
     }
     plot <- plot + 
-        scale_y_continuous(breaks=c(0, .25, .5, .75, 1), name="Relative Yield", limits=c(0, 1.05)) + 
+        scale_y_continuous(breaks=c(0, .25, .5, .75, 1), name="Relative Yield", limits=c(0, 1.1)) + 
         scale_x_continuous("", breaks=c(92, 122, 153, 183, 214, 245), 
                            labels=c("Apr", "May", "Jun", "Jul", "Aug", "Sept")) + 
         theme_bw() + 
