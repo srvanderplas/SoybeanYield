6b6e182cf0bb5da762893ab0c7f93d24151dad40
Susan VanderPlas <srvanderplas@gmail.com>
2015-01-20 18:47:24 -0600

    UI updates

--- ui.R
+++ ui.R
@@ -14,19 +14,24 @@ shinyUI(fluidPage(
   
   wellPanel(
     fluidRow(
-      column(4, offset=1, p("Select a variable to compare up to 3 different values for that variable. You may only select one variable at a time to compare in this way.")),
-      column(3, offset=2, radioButtons("compare", label="Comparison Variable", 
+      column(4, 
+             br(),
+             p("Select a variable to compare up to 3 different values for that variable. You may only select one variable at a time to compare in this way."),
+             br(),
+             p("Click on the boxes below to see what options are available."),
+             p("You can type or select options to add them (use backspace to remove a selected option)."),
+             p("You may select up to three values for the variable chosen as the comparison variable.")),
+      
+      column(2, radioButtons("compare", label="Comparison Variable", 
                              choices=c("Location"="Location", "Planting Date"="PlantDay", "Maturity Group"="MG"), 
-                             inline=TRUE))
-    ), br(),
-    fluidRow(
-      column(4, offset=1, p("Click on the boxes below to see what options are available."),
-                p("You can type or select options to add them (use backspace to remove a selected option)."),
-                p("You may select up to three values for the variable chosen as the comparison variable.")),
-      column(2,div(align="center", uiOutput("location"))),
-      column(2,div(align="center", uiOutput("planting"))),
-      column(2,div(align="center", uiOutput("maturity")))
-    )),
+                             inline=F)),
+      column(2, 
+             div(align="center", uiOutput("location")),
+             div(align="center", uiOutput("planting")),
+             div(align="center", uiOutput("maturity"))),
+      column(4, div(align="center", helpText("Logo(s) go here")))
+    )
+  ), 
   fluidRow(
     column(10, 
       plotOutput("DevelopmentPlot", height=400),
@@ -49,11 +54,11 @@ shinyUI(fluidPage(
            div(align="center", helpText(strong("Fig. 2"), "Relationship between relative yield and soybean cultivar for a specific planting date(s) and location(s). Vertical dotted line(s) indicates the appropriate maturity group for use in a specific location and/or planting date."))), 
     column(2, br(),br(),
            wellPanel(
-             h4("Plot Options"),
+             strong("Plot Options"),
              h5("Relative Yield by MG Plot:"),
              radioButtons("plottype2", "Type", choices=c("Fitted Line"=2, "Box Plot" = 1), selected=2),
              br(),
-             h5("Both Plots:"),
+             strong("Both Plots:"),
              checkboxInput("points", label="Show Points"),
              checkboxInput("failed", label="Include Failed Trials?"),
              checkboxInput("newdata2", "Show 2014 measured data (if available)", value=FALSE),
