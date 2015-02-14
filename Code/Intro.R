62dbb66c533c7ce85ec728238048914bbbbe30be
Susan VanderPlas <srvanderplas@gmail.com>
2015-02-13 18:31:53 -0600

    Fix figure names

--- Code/Intro.R
+++ Code/Intro.R
@@ -1,11 +1,10 @@
 # R code to generate collapsible panel groups. 
-
-
 library(shiny)
 library(htmltools)
 library(plyr)
 library(magrittr)
 library(stringr)
+
 # <img src="photo I.png" width='80%' height='auto'>
 #   <img src="photo II soybean experiment.png" width='80%' height='auto'>
 #   <img src="Photo III SoybeanEmergen.png" width='80%' height='auto'>
@@ -17,10 +16,10 @@ paper <- data.frame(
   captions = readLines("./Data/captions.txt"),
   stringsAsFactors=F
   )
+
 paper$unique <- 1
 paper$unique[paper$title==""] <- 0
 paper$idx <- cumsum(paper$unique)
-
 
 paper.condensed <- ddply(paper, .(idx), function(df){
   data.frame(
@@ -33,23 +32,27 @@ paper.condensed <- ddply(paper, .(idx), function(df){
 paper.condensed$text <- paper.condensed$text %>%
   str_replace(pattern="<br><p align='center'></p>$", replacement="")
 
-
 paper.condensed$headings <- sprintf("heading%02d", paper.condensed$idx)
 paper.condensed$href <-  sprintf("#collapse%02d", paper.condensed$idx)
 paper.condensed$aria.controls <- sprintf("collapse%02d", paper.condensed$idx)
 
 list.of.panels <- 
   sprintf(
-    '<div class="panel panel-default">
+    '<div class="panel panel-success">
         <div class="panel-heading" role="tab" id="%s">
           <h4 class="panel-title">
               <a href="%s" data-toggle="collapse" data-parent="#accordion" aria-expanded="false" aria-controls="%s">%s</a>
           </h4>
         </div>
-        <div id="%s" class="panel-collapse collapse in", role="tabpanel" aria-labelledby="%s">
+        <div id="%s" class="panel-collapse collapse", role="tabpanel" aria-labelledby="%s">
           <div class="panel-body"> %s </div>
         </div>
      </div>', paper.condensed$headings, paper.condensed$href, paper.condensed$aria.controls, paper.condensed$title, paper.condensed$aria.controls, paper.condensed$headings, paper.condensed$text)
+
+list.of.panels[1] <- list.of.panels[1] %>% 
+  str_replace(pattern="panel-collapse collapse", replacement="panel-collapse collapse in") %>%
+  str_replace(pattern='aria-expanded="false"', replacement='aria-expanded="true"') 
+  
 
    
     
@@ -62,7 +65,7 @@ list.of.panels <-
 # </a>
 #   </h4>
 #   </div>
-#   <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+#   <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
 #   <div class="panel-body">
 #   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 # </div>
